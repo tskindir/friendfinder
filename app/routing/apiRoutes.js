3103ed49c741ffc7a6980ae86c3543174e026580
Eskindir <tskindir@gmail.com>
2019-01-28 14:03:08 -0600

    file structure changes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,42 @@
+
+
+var tableData = require("../data/tableData");
+var waitListData = require("../data/waitinglistData");
+
+module.exports = function(app) {
+
+
+  app.get("/api/tables", function(req, res) {
+    res.json(tableData);
+  });
+
+  app.get("/api/waitlist", function(req, res) {
+    res.json(waitListData);
+  });
+
+  app.post("/api/tables", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+    if (tableData.length < 5) {
+      tableData.push(req.body);
+      res.json(true);
+    }
+    else {
+      waitListData.push(req.body);
+      res.json(false);
+    }
+  });
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  app.post("/api/clear", function(req, res) {
+    // Empty out the arrays of data
+    tableData.length = [];
+    waitListData.length = [];
+
+    res.json({ ok: true });
+  });
+};
